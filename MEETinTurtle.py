9d580b9310e7025f1b04e4d10ebfdf848b0f71e8
sumayah20-meet <sumayah20@meet.mit.edu>
2018-07-29 11:14:03 -0400

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,77 @@
+
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+
+turtle.penup() #Pick up the pen so it doesn’t 
+               #draw
+turtle.goto(-200,-100) #Move the turtle to the 
+ #position (-200, -100) 
+ #on the screen
+turtle.pendown() #Put the pen down to start
+                 #drawing
+#Draw the M:
+turtle.goto(-200,-100+200) 
+turtle.goto(-200+50,-100) 
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100) 
+
+
+turtle.penup() #Pick up the pen so it doesn’t 
+               #draw               #draw
+turtle.goto(-50,-100) #Move the turtle to the 
+ #position (-50,-100) 
+ #on the screenand end it before the next line.
+turtle.pendown() #Put the pen down to start
+                 #drawing 
+               #draw
+turtle.goto(-50,-100+200) #Move the turtle to the 
+ #position (-200, -100) 
+ #on the screen
+turtle.pendown() #Put the pen down to start
+                 #drawing
+turtle.goto(0,100) #Move the turtle to the 
+ #position (-200,-100)
+ #on the screen 
+turtle.penup() #Pick up the pen so it doesn’t 
+               #draw
+turtle.goto(-50,-100+200) #Move the turtle to the 
+ #position (-200, -100)
+turtle.penup() #Pick up the pen so it doesn't
+               #draw
+
+turtle.goto(-50,0) #Move the turtle to the 
+ #position (-200, -100) 
+ #on the screen
+turtle.pendown() #Put the pen down to start
+                 #drawing
+turtle.goto(0,0) #Move the turtle to the 
+ #position (-200, -100) 
+ #on the screen
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(0,-100)
+
+turtle.penup()
+turtle.goto(50,-100)
+turtle.pendown
+turtle.goto(70,-100)
+turtle.pendown()
+turtle.goto(70,100)
+turtle.pendown()
+turtle.goto(125,100)
+turtle.penup
+turtle.goto(70,100)
+turtle.penup
+turtle.goto(70,0)
+turtle.pendown
+turtle.goto(125,0)
+
+
+
+turtle.mainloop()
